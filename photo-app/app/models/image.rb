54afe113f5fbb692c18c392e394da2afe469d8cb
Senna Van Londersele <senna.vanlondersele@student.odisee.be>
2017-12-29 02:47:06 +0100

    Update image listing page + show image page

--- photo-app/app/models/image.rb
+++ photo-app/app/models/image.rb
@@ -2,4 +2,12 @@ class Image < ApplicationRecord
   belongs_to :user
   #Use carrierwave gem to link the image model
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+  def picture_size
+  	if picture.size > 5.megabytes
+  		errors.add(:picture, "should be less than 5MB")
+  	end
+  end
 end
